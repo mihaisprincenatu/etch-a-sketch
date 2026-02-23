437adefb1bcdece16d0103de019144d7e595b1ba
Mihai Sprincenatu <250979906+mihaisprincenatu@users.noreply.github.com>
2026-02-23 17:41:42 +0200

    Add logic for letting the user create a new grid

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("button");
 
 function generateGrid(gridSize) {
+  container.textContent="";
+  gridSize = Math.min(gridSize, 100);
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
       const gridItem = document.createElement("div");
@@ -13,5 +16,16 @@ function generateGrid(gridSize) {
     }
   }
 }
+
+button.addEventListener("click", (e) => {
+  let newGridSize = +prompt("Enter grid size (up to 100):", 16);
+  while (isNaN(newGridSize)) {
+    newGridSize = +prompt("Not a number, try again. Enter grid size (up to 100):", 16);
+  }
+  while (newGridSize <= 0) {
+    newGridSize = +prompt("Positive values only. Enter grid size (up to 100):", 16);
+  }
+  generateGrid(newGridSize);
+})
 
 generateGrid(16);
