4732b9c9b80c9c700fff242e4f2cf284b8a5f31f
Mihai Sprincenatu <250979906+mihaisprincenatu@users.noreply.github.com>
2026-02-23 16:39:39 +0200

    Add logic for creating the grid

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+const container = document.querySelector("#container");
 
+function generateGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const gridItem = document.createElement("div");
+      gridItem.classList.add("grid-item");
+      gridItem.style.flexBasis = `${100 / gridSize}%`
+      container.appendChild(gridItem)
+    }
+  }
+}
+
+generateGrid(16);
